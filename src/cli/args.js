b682722a3ba2093510cd68d19f9d22753c51982d
DD <sdsd>
2024-01-27 21:46:00 +0300

    implemented module cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const res = [];
+  while (args.length > 0) {
+    const name = args.shift().slice(2);
+    const val = args.shift();
+    res.push(`${name} is ${val}`);
+  }
+  console.log(res.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,7 @@
 const parseEnv = () => {
-    // Write your code here 
+  const vars = Object.keys(process.env).filter((e) => e.startsWith("RSS_"));
+  const res = vars.map((e) => `${e}=${process.env[e]}`).join(";");
+  console.log(res);
 };
 
 parseEnv();
