1301a560c26887ce7a7400f95bd394b6e28234ce
DD <sdsd>
2024-01-27 22:39:17 +0300

    implemented module hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,11 @@
+import fs from "fs/promises";
+import cr from "crypto";
+
+const file = "./files/fileToCalculateHashFor.txt";
 const calculateHash = async () => {
-    // Write your code here 
+  const text = await fs.readFile(file, "utf8");
+  const hash = cr.createHash("sha256").update(text).digest("hex");
+  console.log(hash);
 };
 
 await calculateHash();
